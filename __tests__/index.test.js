6c04c3b1311b9d474eb416a4101f5270664ac0be
Dmitry Malofeev <dim2k2006@yandex.ru>
2021-04-19 22:59:15 +0300

    Update "loadPage" function

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,6 +3,7 @@ import os from 'os';
 import path from 'path';
 import { promises as fs } from 'fs';
 import nock from 'nock';
+import cheerio from 'cheerio';
 import loadPage from '../src';
 
 let tmpDir;
@@ -45,4 +46,38 @@ describe('page-loader', () => {
 
     await expect(loadPage('https://ru.hexlet.io/courses', 'non-existing-folder')).rejects.toThrow('Error during file saving');
   });
+
+  // прямой тест на функциональность - сохранение картинки
+  test('Downloads page images, saves it to the asset folder and updates images links in html file.', async () => {
+    const page = await readFile(getFixturePath('ru-hexlet-io-courses-with-image.html'));
+    const expectedPage = await readFile(getFixturePath('ru-hexlet-io-courses-with-image-expected.html'));
+
+    nock('https://ru.hexlet.io')
+      .get('/courses')
+      .reply(200, page);
+
+    nock('https://ru.hexlet.io')
+      .get('/assets/professions/nodejs.png')
+      .replyWithFile(200, getFixturePath('nodejs.png'), {
+        'Content-Type': 'image/png',
+      });
+
+    const filepath = await loadPage('https://ru.hexlet.io/courses', tmpDir); // вот этот фалй должен содержать измененные ссылки
+
+    const { dir } = path.parse(filepath);
+    const assetsFolderName = path.basename(filepath, '.html');
+    const assetsFolderPath = path.join(dir, `${assetsFolderName}_files`);
+    const assetFilePath = path.join(assetsFolderPath, 'ru-hexlet-io-assets-professions-nodejs.png');
+    const processedPage = await readFile(filepath);
+
+    await expect(fs.access(assetsFolderPath)).resolves.toBe(undefined);
+    // await expect(fs.access(assetFilePath)).resolves.toBe(undefined);
+    expect(processedPage).toBe(cheerio.load(expectedPage).html());
+  });
+
+  // обработать ошибку создания папки для ассетов
+
+  // обработка ошибки скачивания файла
+
+  // обработка ошибки сохранения файла
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import axios from 'axios';
 import { promises as fs } from 'fs';
 import path from 'path';
+import cheerio from 'cheerio';
+
+const isLocalAsset = (assetUrl, pageUrl) => {
+  if (assetUrl.startsWith('/')) return true;
+
+  const asset = new URL(assetUrl);
+  const page = new URL(pageUrl);
+
+  return asset.hostname === page.hostname;
+};
+
+const getBaseUrl = (pageUrl) => {
+  const url = new URL(pageUrl);
+
+  return `${url.protocol}${url.host}`;
+};
 
 const getUrlWithoutProtocol = (resourceUrl) => {
   const url = new URL(resourceUrl);
@@ -31,10 +47,8 @@ const isLetter = (char) => RegExp(/[a-z]/, 'i').test(char);
 
 const isDigit = (char) => RegExp(/[0-9]/).test(char);
 
-const genPageFilename = (pageUrl) => {
-  const newUrl = getUrlWithoutProtocol(pageUrl);
-
-  const filename = newUrl
+const transformString = (string) => {
+  const result = string
     .split('')
     .map((char) => {
       if (isLetter(char)) return char;
@@ -45,12 +59,26 @@ const genPageFilename = (pageUrl) => {
     })
     .join('');
 
-  return `${filename}.html`;
+  return result;
+};
+
+const getAssetFileName = (assetUrl, baseUrl) => {
+  const absoluteAssetUrl = new URL(assetUrl, baseUrl).pathname; // /assets/professions/nodejs.png
+  const { dir, base } = path.parse(absoluteAssetUrl);
+  const result = `${transformString(`${getUrlWithoutProtocol(baseUrl)}${dir}`)}-${base}`; // ru-hexlet-io-assets-professions-nodejs.png
+
+  return result;
 };
 
 const loadPage = (pageUrl, destPath = process.cwd()) => {
-  const filename = genPageFilename(pageUrl);
-  const filepath = path.join(destPath, filename);
+  const baseUrl = getBaseUrl(pageUrl);
+  const fullUrl = getUrlWithoutProtocol(pageUrl);
+
+  const pageFileName = `${transformString(fullUrl)}.html`;
+  const assetsFolderName = `${transformString(fullUrl)}_files`;
+
+  const pageFilePath = path.join(destPath, pageFileName);
+  const assetsFolderPath = path.join(destPath, assetsFolderName);
 
   return axios
     .get(pageUrl)
@@ -58,11 +86,51 @@ const loadPage = (pageUrl, destPath = process.cwd()) => {
     .catch((error) => {
       throw new Error(`Error during page downloading. ${error}`);
     })
-    .then((data) => fs.writeFile(filepath, data, 'utf-8'))
+    .then((data) => {
+      const $ = cheerio.load(data);
+      const images = Array
+        .from($('img'))
+        .map((element) => $(element))
+        .filter(($element) => {
+          const assetUrl = $element.attr('src');
+
+          return isLocalAsset(assetUrl, pageUrl);
+        });
+
+      const assetsLinks = images.map(($element) => $element.attr('src'));
+
+      images.forEach(($element) => {
+        // assetUrl - /assets/professions/nodejs.png
+        // assetUrl - https://ru.hexlet.io/assets/professions/nodejs.png
+        const assetUrl = $element.attr('src');
+        const assetFileName = getAssetFileName(assetUrl, baseUrl);
+        const assetFilePath = path.join(assetsFolderName, assetFileName);
+
+        $element.attr('src', assetFilePath);
+      });
+
+      const newData = $.html();
+
+      return { data: newData, assetsLinks };
+    })
+    .then(({ data, assetsLinks }) => new Promise((resolve, reject) => {
+      if (assetsLinks.length === 0) {
+        resolve({ data });
+
+        return;
+      }
+
+      fs.mkdir(assetsFolderPath)
+        .then(() => resolve({ data, assetsLinks }))
+        .catch((error) => reject(error));
+    }))
+    // скачать ассеты
+    // обработать ошибки скачивания ассетов (должна ли ошибка скачивания ресурса прерывать весь процесс??)
+    .then(({ data }) => fs.writeFile(pageFilePath, data, 'utf-8'))
     .catch((error) => {
       throw new Error(`Error during file saving. ${error}`);
     })
-    .then(() => path.resolve(filepath));
+    .then(() => path.resolve(pageFilePath));
 };
 
 export default loadPage;
